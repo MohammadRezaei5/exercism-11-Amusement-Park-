04b54442b50f9ec9eabbf2de41886b349f85f056
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-03 11:01:56 +0330

    Function: createVisitor

--- amusement-park.js
+++ amusement-park.js
@@ -1,2 +1,12 @@
 // look back again 🌹❤😒
 
+function createVisitor(name, age, ticketId) {
+  const visitor = {
+    name: name,
+    age: age,
+    ticketId: ticketId,
+  };
+  return visitor;
+}
+
+// console.log(createVisitor("Verena Nardi", 45, "H32AZ123"));
