d5c7fb41a2705aa3fa856ff09796b6606546ad2f
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-03 11:02:20 +0330

    Function: revokeTicket

--- amusement-park.js
+++ amusement-park.js
@@ -10,3 +10,18 @@ function createVisitor(name, age, ticketId) {
 }
 
 // console.log(createVisitor("Verena Nardi", 45, "H32AZ123"));
+
+///---///
+
+// const visitor = {
+//   name: "Verena Nardi",
+//   age: 45,
+//   ticketId: "H32AZ123"
+// };
+
+function revokeTicket(visitor) {
+  visitor.ticketId = null;
+  return visitor;
+}
+
+// console.log(revokeTicket(visitor));
