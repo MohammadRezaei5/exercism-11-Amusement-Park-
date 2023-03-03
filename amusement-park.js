985574a331c400c1002fd3d8c160ac5fbc633cbd
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-03 11:03:52 +0330

    Function: gtcVersion

--- amusement-park.js
+++ amusement-park.js
@@ -71,3 +71,28 @@ function simpleTicketStatus(tickets, ticketId) {
 // console.log(simpleTicketStatus(tickets, "23LA9T41"));
 // console.log(simpleTicketStatus(tickets, "0H2AZ123"));
 // console.log(simpleTicketStatus(tickets, "RE90VAW7"));
+
+///---///
+
+// const visitorNew = {
+//   name: "Verena Nardi",
+//   age: 45,
+//   ticketId: "H32AZ123",
+//   gtc: {
+//     signed: true,
+//     version: "2.1"
+//   }
+// };
+
+// const visitorOld = {
+//   name: "Verena Nardi",
+//   age: 45,
+//   ticketId: "H32AZ123"
+// };
+
+function gtcVersion(visitor) {
+  return visitor?.gtc?.version;
+}
+
+// console.log(gtcVersion(visitorNew));
+// console.log(gtcVersion(visitorOld));
